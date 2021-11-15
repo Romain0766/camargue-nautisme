79666b13f3bae9eb9118bfadf672fcc8e511beb5
Tanatos34 <vincentszyp@live.fr>
2021-11-15 12:18:59 +0100

    msg

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
 
 <body>
     <footer>
-        <div id="carte" >
+            <nav>
+                <ul>
+                    <a href="./index.php"><li>Home</li></a>
+                    <a href="./lieu.php"><li>Lieu</li></a>
+                    <a href="./reservation.php"><li>Reservation</li></a>
+                    <a href="./equipement.php"><li>Equipement</li></a>
+                   
+                </ul>
+        </nav>
+        <div class="carte" >
         <iframe src="https://www.google.com/maps/d/embed?mid=1N51aSzxASDj1ftwV3cw8Nf7dO5YB310m" width="200" height="150" ></iframe>
         </div>
     </footer>
