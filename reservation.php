e140f83c4495efa5fff0e5c39fb3dc33c4af84e2
Romain <romain.calvignac@ecoles-wis.net>
2021-11-25 19:55:51 +0100

    update

--- reservation.php
+++ reservation.php
@@ -10,21 +10,32 @@
     }
 
     else {
-        echo "Bravo vous avez rempli tout les champs";
+        // echo "Bravo vous avez rempli tout les champs";
         // print_r($_POST);
+        // echo"bonjour";
 
-        $req_inst="INSERT iNTO reservation VALUES(NULL,?,?,?,?,?,?,?,?,?)";
+        $req_inst="INSERT iNTO reservation (r_id,r_date,r_nom_client,r_mail_client,
+        r_telephone,r_heure_arr,fk_e_equipement,fk_l_id) VALUES(NULL,?,?,?,?,?,?,?)";
+        
+        // $req_inst ='INSERT iNTO reservation (r_id,r_date,r_nom_client,r_mail_client, r_telephone,
+        // r_heure_arr,fk_e_equipement,fk_l_id) VALUES(NULL,10-10-10,"calvi","de@e.com","03030303",10-10,1,1)';
+        
         require('include/connexion.php');
         $sth= $conn ->prepare($req_inst);
-        $sth -> execute([$_POST['date'],$_POST['nom'],$_POST['monmail'],$_POST['telephone'],
-        $_POST['lieu'],$_POST['heure'],$_POST['equipement']]);
-        $conn = NULL;
+        $parametre=[$_POST['date'],$_POST['nom'],$_POST['monmail'],$_POST['telephone']
+        ,$_POST['heure'],$_POST['equipement'],$_POST['lieu']];
+        if($sth -> execute($parametre) ){
+            // echo "insertion réussi";
     }
+    // else {
+    //     echo 'insertion ko';
+    // }
 }
 
 if (isset($_POST ['monmail']) AND filter_var($_POST ['monmail'], FILTER_VALIDATE_EMAIL)){
-    echo "Le mail n'est pas bon";
+    // echo "Le mail est bon";
 } 
+    }
 
 
 
@@ -34,16 +45,24 @@ if (isset($_POST ['monmail']) AND filter_var($_POST ['monmail'], FILTER_VALIDATE
         <p> Tous les champs sont obligatoires</p>
             <div class="formulaire">
                  <?php
-                        $req='SELECT l_id, l_nom_agence From lieu_location';
-                        $lieu_location= $conn->query($req);
+                        $req_l='SELECT l_id, l_nom_agence From lieu_location';
+                        $lieu_location= $conn->query($req_l);
+                        $lieu_x= $lieu_location->fetchAll(PDO::FETCH_ASSOC);
+                        // print_r($lieu_x);
                         
-                        foreach($lieu_location as $location){
+                        foreach($lieu_x as $location){
                         ?>
 
-               <div class="row">
-                    <label for="<?=$location['l_id']?>"><?=$location['l_nom_agence']?></label>
-                    <input type="radio" name="lieu" id="<?=$location['l_id']?>" value="<?=$location['l_id']?>">
-               </div>
+               <!-- <div class="row form-check">
+                    <input class="form-check-input" type="radio" name="lieu" id="<?=$location['l_id']?>" value="<?=$location['l_id']?>"> 
+                    <label class="form-check-label"  for="<?=$location['l_id']?>"><?=$location['l_nom_agence']?></label>
+               </div> -->
+<!-- boodstrop raido -->
+               <div class="form-check">
+  <input class="form-check-input" type="radio" name="lieu"  id="<?=$location['l_id']?>" value="<?=$location['l_id']?>"  >
+  <label class="form-check-label"  for="<?=$location['l_id']?>"><?=$location['l_nom_agence']?></label>
+</div>
+
                <?php
                     } 
                     ?>
@@ -53,14 +72,14 @@ if (isset($_POST ['monmail']) AND filter_var($_POST ['monmail'], FILTER_VALIDATE
                     <input type="date" size="25" name="date" id="date" placeholder="date de réservation" value="">
                </div>
 
-               <div class="row">
-                    <label for="">Votre nom</label>
-                    <input type="text" size="25" name="nom" id="lanom" placeholder="Votre nom ici"   value="romain">
+               <div class="row form-floating mb-3">
+                    <input class="form-control" type="text" size="25" name="nom" id="lanom" placeholder="Votre nom ici">
+                    <label for="floatingInput">Votre nom</label>
                </div>
         
-            <div class="row">
+            <div class="row form-floating mb-3">
+                <input class="form-control" type="email" id="lamail" size="30" name="monmail" placeholder="Votre mail ici" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
                 <label for="Votre email">Votre email</label>
-                <input type="email" id="lamail" size="30" name="monmail" placeholder="Votre mail ici" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" value="de@dez.fr">
                    
             </div>
 
@@ -77,8 +96,8 @@ if (isset($_POST ['monmail']) AND filter_var($_POST ['monmail'], FILTER_VALIDATE
                 <select name="equipement" id="equipement-select">
                 <option value="-1">--Faire un choix--</option>
                     <?php
-                        $req='SELECT e_id, e_type From equipement';
-                        $type_equipement= $conn->query($req);
+                        $req_e='SELECT e_id, e_type From equipement';
+                        $type_equipement= $conn->query($req_e);
                         
                         foreach($type_equipement as $equipement){
                         ?>
@@ -96,9 +115,11 @@ if (isset($_POST ['monmail']) AND filter_var($_POST ['monmail'], FILTER_VALIDATE
 
         </form>
 
+        <script src="include/boostrap/JS/bootstrap.min.js"></script>
 
     </main>    
 
     <?php
     require('footer.php');
+    $conn = NULL;
 ?>
