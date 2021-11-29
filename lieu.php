f23a6f3e627a6b6ce1f063ee245640ef830b85ca
Tanatos34 <vincentszyp@live.fr>
2021-11-29 10:13:37 +0100

    save

--- lieu.php
+++ lieu.php
@@ -9,13 +9,29 @@
 <div class="main_lieu" > 
     <div class="bandeaux">
         <img class="bandeaux_img" src=".\image\standup-paddle.jpeg" alt="">
-            <p id="lieutexte" >Lorem ipsum dolor sit amet consectetur adipisicing elit. At veritatis quisquam voluptatum ut! Illum necessitatibus excepturi sint
-            </p>
-    </div>
- 
+            <b id="lieutexte" > 
+                <center>  ipsum dolor sit amet consectetur adipisicing elit. At veritatis quisquam voluptatum ut! Illum necessitatibus excepturi sint</center>
+</b>
 </div>
+</div>
+    <div class="lieu_maps" > 
+    <iframe class="maps" src="https://www.google.com/maps/d/embed?mid=1N51aSzxASDj1ftwV3cw8Nf7dO5YB310m" width="400" height="200" ></iframe>
+         
+        <div class="lieu_description">
+        
+                     <p>lieu 1 </p> 
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates recusandae dolorem aliquid iusto, corrupti fuga consectetur minima aliquam distinctio adipisci praesentium sed provident atque quis, doloremque laudantium accusantium a fugiat!
+                    </p>
+                    <p>lieu 2</p> 
+                     <p>
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut harum quam nostrum placeat autem sunt minus facere ipsum commodi, ratione iusto aut obcaecati illum amet, alias eaque! Vero, provident culpa.
+                    </p>
+            
+        
+    </div>
 
-
+</div>
 
     
     </main>    
